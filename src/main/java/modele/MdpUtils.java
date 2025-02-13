91efe3bb154afc511053d5e64893d9ad6bd97785
ange-marie-h <marieange.hoffler@gmail.com>
2025-02-13 20:39:39 +0100

    commit MdpUtils BCrypt 13/02

--- src/main/java/modele/MdpUtils.java
+++ src/main/java/modele/MdpUtils.java
@@ -1,25 +1,24 @@
 package modele;
-import org.mindrot.jbcrypt.BCrypt;
+
+import at.favre.lib.crypto.bcrypt.BCrypt;
 
 public class MdpUtils {
 
     /**
-     * * @param plainPassword, le mot de passe en clair
+     * @param plainPassword, le mot de passe en clair
      * @return le mot de passe crypté
      */
     public static String hashPassword(String plainPassword) {
-        return BCrypt.hashpw(plainPassword, BCrypt.gensalt());
+        return BCrypt.withDefaults().hashToString(12, plainPassword.toCharArray());
     }
 
     /**
      * Permet de vérifier si le mot de passe est correct 
-     * @param plainPassword
-     * @param hashedPassword
+     * @param plainPassword, le mot de passe en clair
+     * @param hashedPassword, le mot de passe crypté
      * @return true si le mot de passe est correct
      */
     public static boolean verifyPassword(String plainPassword, String hashedPassword) {
-        return BCrypt.checkpw(plainPassword, hashedPassword);
+        return BCrypt.verifyer().verify(plainPassword.toCharArray(), hashedPassword).verified;
     }
 }
-
-
