fb73584e15195a4134c0a1b940d3b53dbb91d713
Olivia Rivs <155980126+oliviarives@users.noreply.github.com>
2025-03-24 17:50:42 +0100

    Update RequeteEmployeById.java

--- src/main/java/modele/dao/requetes/Employe/RequeteEmployeById.java
+++ src/main/java/modele/dao/requetes/Employe/RequeteEmployeById.java
@@ -12,12 +12,12 @@ public class RequeteEmployeById extends RequeteEmploye {
         return "SELECT * FROM EMPLOYE WHERE LOGINEMP = ?";
     }
 
-    @Override
+    //@Override
     public void parametres(PreparedStatement prSt, String... id) throws SQLException {
         prSt.setString(1, id[0]);
     }
 
-    @Override
+    //@Override
     public void parametres(PreparedStatement prSt, Employe obj) throws SQLException {
         prSt.setString(1, obj.getLogin());
     }
